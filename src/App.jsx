98eaf05e2575c441162587374893eab5ff4bf185
= <=>
2024-09-23 10:36:59 -0500

    formatted side bar, collapsing when menu icon clicked

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Sidebar } from './Components/Sidebar/Sidebar'
+
+export const App = () => {
+  return (
+    <>
+      <Sidebar/>
+    </>
+  )
+}
+
+export default App
